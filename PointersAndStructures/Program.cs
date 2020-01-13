5bd86f9b0d498f58ed48a0c80bd21783b4f4c6e9
Ediezzle <edmore.mandikiyana@gmail.com>
2020-01-13 11:39:55 +0200

    Add project files.

---
+++ PointersAndStructures/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PointersAndStructures
+{
+    //Pointers can be used with structures if the structure contains ONLY value types as its members
+    struct MyStruct
+    {
+        public int x;
+        public int y;
+        public void SetXY(int i, int j)
+        {
+            x = i;
+            y = j;
+        }
+        public void ShowXY()
+        {
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+        unsafe static void Main(string[] args)
+        {
+            MyStruct ms; //Not necessary to instantiate aan instance of a struct with new keyword
+            MyStruct* ms1 = &ms;
+            ms1->SetXY(10, 20);
+            ms1->ShowXY();
+        }
+    }
+}
